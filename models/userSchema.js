881ddd0bbb06fc5851543f2952a1c7ab6e0b3303
Jeffery Kimbrow <71730781+kimbrow-slice@users.noreply.github.com>
2021-01-14 14:44:48 -0600

    registration sends to DB passwords are not hashed yet
    bcrypt is doing something weird and not hashing the password. Unsure if it is my syntax of what but I will review it tonight and get that figured out.

--- models/userSchema.js
+++ models/userSchema.js
@@ -6,7 +6,6 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username : {type: String, required: true, maxlength: 25},
     password : {type: String, required: true, minlength: 8},
-    password2: {type: String, required: true, minlength: 8},
     email : {type: String, required: true, maxlength: 150},
     admin : {type: Boolean},
     token :{
